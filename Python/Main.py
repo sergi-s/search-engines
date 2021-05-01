c2294a83a4a5a7143993853aa282943f7873a1ff
sergi-s <52626795+sergi-s@users.noreply.github.com>
2021-05-01 13:55:12 +0200

    minor changes

--- Python/Main.py
+++ Python/Main.py
@@ -10,24 +10,15 @@ def Main():
 
 @app.route("/search", methods=['POST', "GET"])
 def search():
-    # ? GET
-    if request.method == "GET":
-        if(request.args.get("search") == "Search"):
-            query = request.args.get("input_query")
-            return "null"
-        if(request.args.get("generate") == "Generate Files"):
-            files = GenearteFiles.Files(numFiles=3, endLetter='f', rangeUpper=5,
-                                        rangeLower=10, useOld=False)
-            return "Generate Files"
-    """------------------------------------------------------------"""
-    # ? POST
     if request.method == "POST":
         if("search" in request.form.keys()):
             query = request.form['input_query']
+            files = GenearteFiles.Files(numFiles=3, endLetter='f', rangeUpper=5,
+                                        rangeLower=10, useOld=True)
             if(request.form["models"] == "statistical"):
-                return GenearteFiles.Search_Statistical(query)
+                return GenearteFiles.Search_Statistical(files, query)
             elif(request.form["models"] == "vectorspace"):
-                return GenearteFiles.Search_VectorSpace(query)
+                return GenearteFiles.Search_VectorSpace(files, query)
 
         elif("generate" in request.form.keys()):
             files = GenearteFiles.Files(numFiles=3, endLetter='f', rangeUpper=5,
